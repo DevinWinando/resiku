745cfbf75576915ba77ed79cc78ff8d18fa90b22
Devin Winando <devin.winando13@gmail.com>
2022-01-28 21:52:00 +0700

    add: routing

--- pages/courier/[courier].tsx
+++ pages/courier/[courier].tsx
@@ -1,15 +1,21 @@
-import Head from "next/head";
 import { useRouter } from "next/router";
-import styles from "../styles/Home.module.css";
 import { Truck, Package, Moon } from "phosphor-react";
 
 interface props {
   couriers: Array<any>;
 }
 
-function Home({ couriers }: props) {
-  const router = useRouter();
+interface Courier {
+  code: string;
+  description: string;
+}
 
+interface CourierDetail {
+  courier: Courier;
+}
+
+function Courier({ couriers }: props): JSX.Element {
+  const router = useRouter();
   return (
     <>
       <div className="navbar justify-center shadow-lg bg-neutral text-neutral-content">
@@ -31,7 +37,7 @@ function Home({ couriers }: props) {
           <h1 className="font-bold text-2xl text-center mt-4">Cek Resimu</h1>
           <Package size={300} weight="duotone" className="mt-4" />
           <div className="ekspedisi flex flex-wrap justify-self-center gap-2 mt-4 px-4 justify-center">
-            {couriers.map((courier) => (
+            {couriers.map((courier: Courier) => (
               <button className="btn" key={courier.code} onClick={() => router.push(`/courier/${courier.code}`)}>
                 {courier.description}
               </button>
@@ -39,7 +45,7 @@ function Home({ couriers }: props) {
           </div>
           <div className="form-control w-3/4 mt-4">
             <div className="relative">
-              <input placeholder="Cari..." className="w-full pr-16 input input-bordered" type="text" disabled />
+              <input placeholder="Cari..." className="w-full pr-16 input input-bordered" type="text" />
               <button className="absolute top-0 right-0 rounded-l-none btn btn-info">Cek Resi</button>
             </div>
           </div>
@@ -60,4 +66,19 @@ export async function getStaticProps() {
   };
 }
 
-export default Home;
+// create function getStaticPaths
+export async function getStaticPaths() {
+  const response = await fetch("https://api.binderbyte.com/v1/list_courier?api_key=65e6e9dc1f8678e0c44272f055cee0a08056db9e7c9c765c60f7b7e1f696ed97");
+  const couriers = await response.json();
+  const paths = couriers.map((courier: Courier) => ({
+    params: {
+      courier: courier.code,
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default Courier;
